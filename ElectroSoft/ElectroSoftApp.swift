1b8ba1eb65b9ea1c37cb8b8a92e2935f2897ce61
Anukriti-j <anu200307@gmail.com>
2025-11-27 11:03:19 +0530

    Initial Commit

---
+++ ElectroSoft/ElectroSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroSoftApp.swift
+//  ElectroSoft
+//
+//  Created by coditas on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ElectroSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
